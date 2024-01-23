99455f56c6081ec31f13eff0fdc63f277e36e4c7
nixnixvero <156133618+nixnixvero@users.noreply.github.com>
2024-01-22 23:59:06 -0800

    Add files via upload
    I was trying to do it as the example in the first week as Hello World!

--- Journal_prompt_1.py
+++ Journal_prompt_1.py
@@ -1,2 +1,6 @@
-print("Veronica Sanchez\nMy favorite movie is 'Shrek'\nSome of my favorite dishes are soups and baleadas")
+#This is a comment
+#This program will write My name is Veronica Sanchez and my favorite movie is 'Sherk'. Some of my favorites dishes are soups and baleadas.
 
+def myname():
+	print("My name is Veronica Sanchez and my favorite movie is Sherk. Some of my favorites dishes are soups and baleadas")
+print(myname()) 
