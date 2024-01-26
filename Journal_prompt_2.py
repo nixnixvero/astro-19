ef11cb38743f0cf11c4c5094fa0a10246f95c31c
nixnixvero <156133618+nixnixvero@users.noreply.github.com>
2024-01-25 17:42:14 -0800

    Add files via upload
    Journal prompt 2

--- Journal_prompt_2.py
+++ Journal_prompt_2.py
@@ -1,11 +1,11 @@
-x = 5.0 + 8.3
+x = 5.0 + 8.3    #sum of floats
 print(x)
 print(type(x)) 
 
-x = 6 - 3
+x = 6 - 3        #subtration of integer
 print(x)
 print(type(x))
 
-x = 3.9 * 9
-print(x)
-print(type(x))
+x = 3.9 * 9      #the product of a floating point number and an integer
+print(x)         #calculate the product
+print(type(x))   #print the product
